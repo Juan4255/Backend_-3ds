3dc9c6020cb5f8a6ff68432ee20841cbcd153369
Juan4255 <juan.zandonai3@gmail.com>
2026-02-27 13:58:06 +0000

    Código If else e modificação para ternário

---
+++ variaveis/variavel-pessoa.js
@@ -0,0 +1,8 @@
+// var, let, const
+//valores constantes e variáveis
+let nome = "Petter"
+let sobrenome = "Van Helsing" 
+const cpf = "899.888.999-99"
+const rg = "101010"
+let idade = "39"
+let solteiro = "true"
