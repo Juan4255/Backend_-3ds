a545c205c859cf1bad5b79ebcbb3b7804cd02fd7
Juan4255 <juan.zandonai3@gmail.com>
2026-02-27 13:58:06 +0000

    Código If else e modificação para ternário

---
+++ condicionais/if.else.js
@@ -0,0 +1,24 @@
+ // if, else, else if, ternário
+ // >, <, >=, !=
+ // ||, &&]
+
+ let nome = "Julius"
+ let idade =34
+
+
+ if (idade > 110) {
+    console.log("Proibido a entrada")
+    console.log("Favor trazer acompanhante!!!")
+ } else if (idade >= 17 && idade <= 110) {
+    console.log("Pode entrar")
+    console.log("Festa indicada: Balada!!!")
+} else {
+  console.log("Vaza, menor!")
+  console.log("Festa indicada: Pijama!!!") 
+  console.log("") 
+ }
+ 
+   
+
+   
+ 
