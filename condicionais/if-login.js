fbedae1d125edf1345a93e8b2b83fb47dfc9fece
Juan4255 <juan.zandonai3@gmail.com>
2026-03-05 14:15:24 +0000

    Finalizar if- else e introdução aos Arrays

---
+++ condicionais/if-login.js
@@ -0,0 +1,12 @@
+// Verifica se LOGIN e SENHA
+// correspondem ao mesmo tempo (&&)
+
+ const Login = 'superman'
+ const Senha = 'segredo13'
+  
+ if (Login == 'superman' && Senha == 'segredo123' ) {
+  console.log ('acesso liberado!')    
+ } else {
+  console.warn('verifique seus dados!')
+
+ }
