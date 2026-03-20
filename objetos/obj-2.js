742083063d6d5299625709569355b725e6a3fb7d
Juan4255 <juan.zandonai3@gmail.com>
2026-03-20 13:27:43 +0000

    FInalização de Objetos JS

---
+++ objetos/obj-2.js
@@ -0,0 +1,22 @@
+// Escolher um tema 
+// Criar um objeto (à sua escolha)
+// Deve ter 5 propriedades no mínimo
+// Imprima como no exemplo...
+// ex.: console.log("Tipo de Carne:" + carne.tipo)
+ 
+
+
+ const jogo = {
+ tipo: "ação",
+estaPublicado:true,
+custo: "grátis",
+ehMultiplayer:true,
+ehIntuitivo:true,
+ehP2W:true
+}
+console.log(jogo.tipo)
+console.log(jogo.estaPublicado)
+console.log(jogo.custo)
+console.log(jogo.ehMultiplayer)
+console.log(jogo.ehIntuitivo)
+console.log(jogo.ehP2W)
