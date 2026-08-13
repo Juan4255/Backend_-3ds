3b967baa34e6fa7e5131fcb088343e54007b5500
Juan4255 <juan.zandonai3@gmail.com>
2026-08-13 13:29:58 +0000

    adiciona servidor HTTP e páginas de navegação para compras

--- 2°TRIMESTRE/modulos/mod-exercicio-01/filmes.js
+++ 2°TRIMESTRE/modulos/mod-exercicio-01/filmes.js
@@ -1,3 +1,6 @@
-function alugarFilme (filme, preço){
+export function alugarFilme (filme, preço){
  return `${filme} foi alugado por:${preço}`
+}
+export function devolverFilme(filme){
+    return `${filme}  o filme foi devolvido `
 }
---
+++ 2°TRIMESTRE/modulos/mod-exercicio-01/main-filme.js
@@ -0,0 +1,6 @@
+import {alugarFilme,devolverFilme} from './filmes.js'
+
+console.log(alugarFilme("Vingadores:Ultimato", 'R$ 12,90'))
+console.log(devolverFilme("Vingadores:Ultimato"))
+console.log(alugarFilme("Homem Aranha 2 ", 'R$ 20'))
+console.log(devolverFilme("Homem Aranha 2 "))
